4ad295694bf75c16e5dcf898d5f7f206851bdded
annplay <messageforannolay@gmail.com>
2025-03-08 21:31:11 +0200

    009

--- src/js/footer.js
+++ src/js/footer.js
@@ -142,37 +142,33 @@ function handleSubmit(event) {
   }
   removeMessage(emailInput);
 }
-
-const modalWindow = document.querySelector('.modal-window');
-const modalWindowClose = document.querySelector('.modal-close');
+// modal window open - close //
+// const formFooterButton = document.querySelector(".footer-button-submit");
+const modalWindow = document.querySelector(".modal-window");
+const modalWindowClose = document.querySelector(".modal-close");
 
 export function addClassIsOpenModal() {
-  return modalWindow.classList.add('is-open');
-}
+    return modalWindow.classList.add("is-open");
+};
 function deleteClassIsOpenModal() {
-  return modalWindow.classList.remove('is-open');
-}
-// const isOpenModalWindow = formFooterButton.addEventListener("click", addClassIsOpenModal); //open modal on submit form
-const modalCloseButton = modalWindowClose.addEventListener(
-  'click',
-  deleteClassIsOpenModal
-); // close moda on click x
+    return modalWindow.classList.remove("is-open");
+};
+const isOpenModalWindow = formFooterButton.addEventListener("click", addClassIsOpenModal); //open modal on submit form
+const modalCloseButton = modalWindowClose.addEventListener("click", deleteClassIsOpenModal); // close moda on click x
 
 //close modal keydown esc
-const modalWindowCloseEsc = document.addEventListener('keydown', event => {
-  if (event.key === 'Escape') {
-    return modalWindow.classList.remove('is-open');
-  }
-});
+const modalWindowCloseEsc = document.addEventListener("keydown", event => {
+   if (event.key === "Escape") {       
+        return modalWindow.classList.remove("is-open");
+    };
+  });
 
 // close modal click backdrop
-const modalWindowCloseBackDrop = modalWindow.addEventListener(
-  'click',
-  event => {
+const modalWindowCloseBackDrop = modalWindow.addEventListener("click",event => {
     const clickedElement = event.target;
     if (clickedElement !== modalWindow) {
-      return;
-    }
-    return modalWindow.classList.remove('is-open');
-  }
-);
+        return;
+}
+    return modalWindow.classList.remove("is-open");
+})
+// modal window open - close //
